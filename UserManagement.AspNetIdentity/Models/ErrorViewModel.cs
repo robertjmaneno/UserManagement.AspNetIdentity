8c0efe9c13a96883f85960aa0d6867b00650d8cc
Robert Maneno <robertjmaneno@gmail.com>
2024-07-31 07:19:03 +0200

    Add project files.

---
+++ UserManagement.AspNetIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserManagement.AspNetIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
