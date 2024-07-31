136408a8ef3ea1afa04361c8c800932e42100a9a
Robert Maneno <robertjmaneno@gmail.com>
2024-07-31 20:01:44 +0200

    Added database configuration and created identity tables

--- UserManagement.AspNetIdentity/Program.cs
+++ UserManagement.AspNetIdentity/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using UserManagement.AspNetIdentity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
