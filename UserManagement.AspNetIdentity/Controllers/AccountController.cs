edaab002e775fc9af669a747a7705ed6d5dc523a
Robert Maneno <robertjmaneno@gmail.com>
2024-08-04 19:30:37 +0200

    Added LoginView

--- UserManagement.AspNetIdentity/Controllers/AccountController.cs
+++ UserManagement.AspNetIdentity/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using UserManagement.AspNetIdentity.Models.Domain;
 using UserManagement.AspNetIdentity.Models.ViewModels;
+using System.Threading.Tasks;
 
 namespace UserManagement.AspNetIdentity.Controllers
 {
@@ -18,11 +19,11 @@ namespace UserManagement.AspNetIdentity.Controllers
 
         public IActionResult Register()
         {
-            RegisterViewModel registerViewModel = new RegisterViewModel();
-            return View(registerViewModel);
+            return View(new RegisterViewModel());
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
             if (ModelState.IsValid)
@@ -41,17 +42,41 @@ namespace UserManagement.AspNetIdentity.Controllers
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return RedirectToAction("Index", "Home");
                 }
-                else
+                foreach (var error in result.Errors)
                 {
-                   
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    }
+                    ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
-
             return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.Email, loginViewModel.Password, loginViewModel.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View(loginViewModel);
         }
     }
 }
